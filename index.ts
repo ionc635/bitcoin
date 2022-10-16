17d46c9aa2d4bf2695d39fbaceb2f4bcdbbf18c0
steve <ionc635@naver.com>
2022-10-16 21:01:00 +0900

    chore: index.ts 삭제

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { FieldElement } from "./src/field.element";
-
-const a = new FieldElement(7, 13);
-const b = new FieldElement(6, 13);
-
-console.log(a === b);
-console.log(a === a);
-console.log(a.equal(b));
